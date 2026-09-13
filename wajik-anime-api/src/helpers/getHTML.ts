de32eba37d413aec5545c33436df32ad1e3d8ca8
zarirulaja <izzaahmad73@gmail.com>
2026-09-14 04:48:41 +0700

    update submodule reference for Chrome 120 UA fix

--- wajik-anime-api/src/helpers/getHTML.ts
+++ wajik-anime-api/src/helpers/getHTML.ts
@@ -2,7 +2,8 @@ import errorinCuy from "./errorinCuy.js";
 import sanitizeHtml from "sanitize-html";
 
 export const userAgent =
-  "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+
 
 
 export default async function getHTML(
